35ed49739a9a6b90277e815f769bc7e2a31f358e
Ukay Hub <johndavelausa@gmail.com>
2026-08-29 23:01:16 +0800

    Fix realtime requirement notifications

--- app/Notifications/VerificationRequirementUploadedNotification.php
+++ app/Notifications/VerificationRequirementUploadedNotification.php
@@ -4,15 +4,11 @@ namespace App\Notifications;
 
 use App\Models\Cadet;
 use App\Models\Document;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Notification;
 
-class VerificationRequirementUploadedNotification extends Notification implements ShouldQueue
+class VerificationRequirementUploadedNotification extends Notification
 {
-    use Queueable;
-
     public function __construct(
         public Cadet $cadet,
         public Document $document
