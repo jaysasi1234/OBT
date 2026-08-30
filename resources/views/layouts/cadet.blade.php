a72eb109a0072f5580219b0a6ce372aec439555d
Ukay Hub <johndavelausa@gmail.com>
2026-08-30 22:24:38 +0800

    fix top avatar

--- resources/views/layouts/cadet.blade.php
+++ resources/views/layouts/cadet.blade.php
@@ -1370,12 +1370,11 @@
                 <!-- PROFILE -->
                 <div class="profile-area" onclick="toggleDropdown()">
                                     <div class="topbar-profile-avatar">
-                                            <img
-                                                src="{{ Auth::user()->profile_picture
-                                                    ? asset('storage/' . Auth::user()->profile_picture) . '?v=' . (Auth::user()->updated_at?->timestamp ?? time())
-                                                    : asset('images/default-avatar.png') }}"
-                                                alt="Profile Photo"
-                                                onerror="this.onerror=null;this.src='{{ asset('images/default-avatar.png') }}';">
+                                        <img
+                                            src="{{ Auth::user()->profile_picture
+                                                ? asset('storage/' . Auth::user()->profile_picture) . '?v=' . (Auth::user()->updated_at?->timestamp ?? time())
+                                                : 'https://ui-avatars.com/api/?name=' . urlencode(Auth::user()->name) }}"
+                                            alt="Profile Photo">
                                     </div>
                                         
 
