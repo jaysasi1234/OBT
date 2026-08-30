5fbbce9368b7347f89d4a406c9ec10e604d7c3f8
Ukay Hub <johndavelausa@gmail.com>
2026-08-30 22:28:23 +0800

    fix profile admin

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -7,7 +7,7 @@
 
     $profilePhoto = $user->profile_picture
         ? Storage::disk('public')->url($user->profile_picture) . '?v=' . $user->updated_at->timestamp
-        : 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png';
+        : asset('images/avatar.png');
 @endphp
 
 @vite(['resources/css/admin/profile.css'])
