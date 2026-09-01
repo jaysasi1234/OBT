4df8393a06fca60163e87793ad80256557c7dd83
Ukay Hub <johndavelausa@gmail.com>
2026-09-01 23:11:43 +0800

    fix super admin pages

--- app/Http/Controllers/SuperAdmin/CadetRequirementController.php
+++ app/Http/Controllers/SuperAdmin/CadetRequirementController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Cadet;
 use App\Models\Batch;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class CadetRequirementController extends Controller
 {
@@ -49,34 +50,85 @@ class CadetRequirementController extends Controller
 
         return response()->json([
             'id' => $cadet->id,
+
             'full_name' => $cadet->full_name,
+
             'trb_control_number' => $cadet->trb_control_number,
+
             'course' => $cadet->course,
 
-            'onboard_requirements' => $cadet->onboardRequirements->map(function ($item) {
+            'onboard_requirements' => $cadet->onboardRequirements
+                ->map(function ($item) {
 
-                return [
-                    'id' => $item->id,
-                    'status' => $item->status,
+                    /*
+                    |--------------------------------------------------------------------------
+                    | SUBMITTED DATE
+                    |--------------------------------------------------------------------------
+                    */
 
-                    'submitted_at' => $item->submitted_at
-                        ? $item->submitted_at->format('M d, Y h:i A')
-                        : null,
+                    $submittedAt = null;
 
-                    'approved_at' => $item->approved_at
-                        ? $item->approved_at->format('M d, Y h:i A')
-                        : null,
+                    if (!empty($item->submitted_at)) {
+                        try {
+                            $submittedAt = Carbon::parse(
+                                $item->submitted_at
+                            )->format('M d, Y h:i A');
+                        } catch (\Throwable $e) {
+                            $submittedAt = null;
+                        }
+                    }
 
-                    'remarks' => $item->remarks,
 
-                    'attachment' => $item->attachment,
+                    /*
+                    |--------------------------------------------------------------------------
+                    | APPROVED DATE
+                    |--------------------------------------------------------------------------
+                    */
 
-                    'requirement' => [
-                        'title' => optional($item->requirement)->title ?? '-',
-                        'frequency' => optional($item->requirement)->frequency ?? '-',
-                    ],
-                ];
-            })->values(),
+                    $approvedAt = null;
+
+                    if (!empty($item->approved_at)) {
+                        try {
+                            $approvedAt = Carbon::parse(
+                                $item->approved_at
+                            )->format('M d, Y h:i A');
+                        } catch (\Throwable $e) {
+                            $approvedAt = null;
+                        }
+                    }
+
+
+                    /*
+                    |--------------------------------------------------------------------------
+                    | RESPONSE
+                    |--------------------------------------------------------------------------
+                    */
+
+                    return [
+                        'id' => $item->id,
+
+                        'status' => $item->status,
+
+                        'submitted_at' => $submittedAt,
+
+                        'approved_at' => $approvedAt,
+
+                        'remarks' => $item->remarks,
+
+                        'attachment' => $item->attachment,
+
+                        'requirement' => [
+                            'title' =>
+                                optional($item->requirement)->title
+                                ?? '-',
+
+                            'frequency' =>
+                                optional($item->requirement)->frequency
+                                ?? '-',
+                        ],
+                    ];
+                })
+                ->values(),
         ]);
     }
 }
